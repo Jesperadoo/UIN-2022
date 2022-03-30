23f7f826b5d47fc23bc8c9b0b0cdb78e937a8d8f
Jesper Borgersen <64968009+Jesperadoo@users.noreply.github.com>
2022-03-30 12:34:28 +0200

    Add files via upload

---
+++ oppgave8/src/lib/client.js
@@ -0,0 +1,19 @@
+import sanityClient from '@sanity/client'
+
+/* const options = {
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  dataset: process.env.REACT_APP_SANITY_DATASET_NAME,
+} */
+
+const options = {
+  projectId: '5sdfscyo',
+  dataset: 'production',
+  apiVersion: '2021-03-25',
+}
+
+const client = sanityClient({
+  ...options,
+  useCdn: process.env.NODE_ENV === 'production',
+})
+
+export default client
